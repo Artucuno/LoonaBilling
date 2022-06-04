047a101ecc003dd07ef0a64d7d8877dc713196e1
Artucuno <artucunov@gmail.com>
2022-06-04 11:25:08 +1000

    testing

--- run.py
+++ run.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'yay it works!'
+    return 'yay it works!!'
+
+@app.route('/admin')
+def admin():
+    return 'admin'
 
 app.run(host='0.0.0.0', port=80, debug=True)
