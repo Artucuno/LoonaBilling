7e18f551329d690defe624593cac27930ff375bf
Artucuno <artucunov@gmail.com>
2022-06-04 13:14:23 +1000

    Removed pycache and updated update script

--- update.py
+++ update.py
@@ -1,5 +1,11 @@
 # Simple script to push updates
 import sys, os
+import shutil
+
+for root, dirs, files in os.walk(".", topdown=False):
+    for name in dirs:
+        if name == '__pycache__':
+            shutil.rmtree(os.path.join(root, name))
 
 os.system('git add -A')
 os.system('git commit -a')
