1fe0f36d9f04da79487f0ba2000e6f607d855441
Artucuno <artucunov@gmail.com>
2022-06-27 19:42:31 +1000

    Fixing vulnerabilities

--- core/modules/payments/stripepay.py
+++ core/modules/payments/stripepay.py
@@ -130,7 +130,7 @@ def adminListPurchases():
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
-            return str(e)
+            #return str(e)
             return redirect(url_for('Stripe.adminListPurchases'))
     try:
         bal = stripe.Balance.retrieve()["available"][0]["amount"]
