81540a4db164242b7444a4297e86424a130c5653
Artucuno <artucunov@gmail.com>
2022-07-19 11:39:24 +1000

    Fixed config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 domain = 'localhost'
 domain_url = f"http://{domain}/"
 ip = '0.0.0.0'
-debug = True
+debug = False
 port = 80
 ssl = None # Replace with ssl = ('cert.pem', 'key.pem') for SSL
 filemanagerEnabled = True
